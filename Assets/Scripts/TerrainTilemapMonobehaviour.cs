dc689b5e74085b74d891a9df16e1cb3dde53e68e
Matthew Goff <mtthwgff@gmail.com>
2022-03-20 18:05:44 -0400

    Tilemap now generates small island

--- Assets/Scripts/TerrainTilemapMonobehaviour.cs
+++ Assets/Scripts/TerrainTilemapMonobehaviour.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class TerrainTilemapMonobehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Tile[] Tiles;
 
-    // Update is called once per frame
-    void Update()
+    public void Start()
     {
+        Tilemap tile_map = gameObject.GetComponent<Tilemap>();
         
+        int width = 20;
+        int height = 20;
+        int radius = 5;
+        Tile tile;
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Vector3Int location = new Vector3Int(x - width / 2, y - height / 2, 0);
+
+                if (location.magnitude < radius)
+                {
+                    tile = Tiles[1];
+                }
+                else
+                {
+                    tile = Tiles[5];
+                }
+
+                tile_map.SetTile(location, tile);
+            }
+        }
     }
 }
