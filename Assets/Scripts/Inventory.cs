8d3ea3da1cc23ad47b4f2740fc7c2f87b08539b8
James Boggs <jmsboggs@gmail.com>
2022-03-26 15:09:39 -0400

    Create Inventory class and add it as an attribute to PersonModel

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Inventory
+{
+    public int Capacity { get; protected set; }
+    public int AmountHeld { get; protected set; }
+    
+    protected Dictionary<ItemType, int> ItemAmounts { get; set; } /// Holds the number of items of each type in this inventory.
+
+    public Inventory(int capacity)
+    {
+        Capacity = capacity;
+        AmountHeld = 0;
+        ItemAmounts = new Dictionary<ItemType, int>();
+    }
+
+    public bool AddItem(ItemType itemType)
+    {
+        if (AmountHeld == Capacity) { return false; }
+
+        if (ItemAmounts.ContainsKey(itemType)) { ItemAmounts[itemType]++; }
+                                          else { ItemAmounts.Add(itemType, 1); }
+        AmountHeld++;
+        return true;
+    }
+
+    public bool RemoveItem(ItemType itemType)
+    {
+        if (!ItemAmounts.ContainsKey(itemType)) { return false; }
+
+        ItemAmounts[itemType]--;
+        if (ItemAmounts[itemType] == 0)
+        {
+            ItemAmounts.Remove(itemType);
+        }
+        AmountHeld--;
+        return true;
+    }
+}
--- Assets/Scripts/PersonModel.cs
+++ Assets/Scripts/PersonModel.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,4 +7,14 @@ public class PersonModel : IWorldEntityModel
     // PUBLIC VARS
     public Vector3Int Position { get; protected set; }
     public PersonMonobehaviour Monobehaviour { protected get; set; }
+
+    // PROTECTED VARS
+    protected Inventory Inventory { get; set; }
+
+    //PUBLIC METHODS
+    public PersonModel(int x, int y)
+    {
+        Position = new Vector3Int(x, y, 0);
+        Inventory = new Inventory(20);
+    }
 }
